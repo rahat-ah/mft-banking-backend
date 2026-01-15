b8214d3a27c305f4ed5cbf2d33b88dbdf270b184
Rahat <rahatahmedbosscomputer@gmail.com>
2026-01-15 16:52:29 +0600

    chenge cors frontend url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
   {
-  origin:process.env.FRONTEND_URL || "http://localhost:5173", 
+  origin:process.env.FRONTEND_URL || "https://mft-banking-frontend.vercel.app", 
   credentials: true,
 }
 ));
