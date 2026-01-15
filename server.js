a8f997c7e5883bd3549136d967b52bba4b8ee1b1
Rahat <rahatahmedbosscomputer@gmail.com>
2026-01-15 14:33:54 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import express from 'express';
+import cookieParser from "cookie-parser";
+import {connectToMongoDb} from './config/mongoDb.js';
+import userRouter from './routes/userRouter.js';
+import cors from 'cors';
+import createDefaultAdmin from './utils/createAdmin.js';
+import authRouter from './routes/authRoutes.js';
+import "./cron/otpCleanup.js"
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors(
+  {
+  origin:process.env.FRONTEND_URL || "http://localhost:5173", 
+  credentials: true,
+}
+));
+app.use(express.json());
+app.use(cookieParser())
+app.use('/auth',authRouter)
+app.use('/user', userRouter);
+
+const startServer = async () => {
+  try {
+    await connectToMongoDb();
+    await createDefaultAdmin();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("❌ Startup error:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
---
+++ utils/createAdmin.js
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt";
+import UserModel, { AdminModel } from "../config/mongoDb.js";
+
+const createDefaultAdmin = async() => {
+  const adminExists = await AdminModel.findOne({ "profile.role": "admin" });
+
+  if (adminExists)  return;
+
+  const users = await UserModel.find()
+  
+  await AdminModel.create({
+    profile: {
+      fullName: process.env.ADMIN_FULL_NAME,
+      email: process.env.ADMIN_EMAIL,
+      mobileNumber: process.env.ADMIN_PHONE,
+      password: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
+      secretCode: await bcrypt.hash(process.env.ADMIN_SECRET, 10),
+      adminCode: await bcrypt.hash(process.env.ADMIN_CODE, 10),
+      role: "admin",
+    },
+    customerLetestSerial:users.length
+  });
+
+  console.log("✅ Admin created from ENV");
+};
+
+export default createDefaultAdmin;
