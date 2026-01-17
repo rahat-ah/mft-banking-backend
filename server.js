786425c5cecc4f7b3e613c0d2fff84441d8e5919
Rahat <rahatahmedbosscomputer@gmail.com>
2026-01-17 12:28:17 +0600

    add allow headers

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"]
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
 }));
 
 app.use(express.json());
