4658de789d389bfe6df8e12ef6cf34815fd3a5eb
Rahat <rahatahmedbosscomputer@gmail.com>
2026-01-18 11:56:42 +0600

    update backend

--- server.js
+++ server.js
@@ -7,39 +7,30 @@ import cors from 'cors';
 import createDefaultAdmin from './utils/createAdmin.js';
 import authRouter from './routes/authRoutes.js';
 import "./cron/otpCleanup.js"
+import path from 'path';
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173" // optional for local dev
-];
+const _dirname = path.resolve()
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if(!origin) return callback(null, true); // allows Postman / server requests
-    if(allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS policy: Origin not allowed"));
-    }
-  },
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"]
-}));
+const corsOption = {
+  origin:process.env.FRONTEND_URL,
+  credentials:true
+}
+
+app.use(cors(corsOption));
 
 app.use(express.json());
 app.use(cookieParser())
 app.use('/auth',authRouter)
 app.use('/user', userRouter);
 
-app.use((err, req, res, next) => {
-  console.error("Global Error:", err);
-  res.status(500).json({ message: "Internal Server Error", error: err.message });
-});
+app.use(express.static(path.join(_dirname,"/mft-banking-frontend/dist")))
+app.all(/.*/,(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"mft-banking-frontend","dist","index.html"))
+})
 
 const startServer = async () => {
   try {
